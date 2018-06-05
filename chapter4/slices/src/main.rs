1c11183e7c098c2763b12093a0f1156420bebc31
Szecsődi Imre <imre56734@hotmail.com>
2018-06-05 16:42:43 +0200

    chapter4 done

--- chapter4/slices/src/main.rs
+++ chapter4/slices/src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
-    println!("Hello, world!");
+    
+    let s = String::from("hello world");
+    // You can slice Strings
+    let hello = &s[0..5];
+    let world = &s[6..11];
+
+    println!("{}", hello);
+    println!("{}", first_word("Hello bello"));
+
+
+    // You can slice lists
+    let a = [1, 2, 3, 4, 5];
+
+    let slice = &a[1..3];
 }
+
+// This is a general approach 
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
