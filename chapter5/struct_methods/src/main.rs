1a6cf0b98e4de890882c97421c2c2d3674935669
Imre Secodi <imre.secodi@gmail.com>
2018-06-06 23:14:51 +0200

    chapter5 done

--- chapter5/struct_methods/src/main.rs
+++ chapter5/struct_methods/src/main.rs
@@ -1,3 +1,4 @@
+// Directive provides a printable Struct
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
@@ -5,8 +6,19 @@ struct Rectangle {
 }
 
 impl Rectangle {
+    // Methods, take self as parameter
     fn area(&self) -> u32 {
         self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+
+    // Associated function, does not take self as parameter
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size}
     }
 }
 
@@ -17,4 +29,14 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         rect1.area()
     );
+
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let square = Rectangle::square(123);
+
 }
