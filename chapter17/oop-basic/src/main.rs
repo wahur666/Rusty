f260263dd2c51f26f72735ac2b7a8d893ce4df4e
Szecsődi Imre <imre.szecsodi@l-mobile.com>
2021-01-23 20:31:29 +0100

    chapter 17 done

---
+++ chapter17/oop-basic/src/main.rs
@@ -0,0 +1,9 @@
+use oop_basic::AveragedCollection;
+
+fn main() {
+    // let mut a = AveragedCollection {
+    //     average: 0.0,
+    //     list: vec![]
+    // };
+   // a.update_average();
+}
