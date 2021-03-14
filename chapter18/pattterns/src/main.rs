d803ae74cf37407a10927062fbdebdcd696522bc
Szecsődi Imre <imre.szecsodi@l-mobile.com>
2021-03-14 21:21:52 +0100

    chapter18 done

---
+++ chapter18/pattterns/src/main.rs
@@ -0,0 +1,49 @@
+fn main() {
+    println!("Hello, world!");
+
+    let favorite_color: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "34".parse();
+
+    if let Some(color) = favorite_color {
+        println!("Using your favorite color, {}, as the background", color);
+    } else if is_tuesday {
+        println!("Tuesday is green day!");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using purple as the background color");
+        } else {
+            println!("Using orange as the background color");
+        }
+    } else {
+        println!("Using blue as the background color");
+    }
+
+    let mut stack = Vec::new();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!("{}", top);
+    }
+
+    let v = vec!['a', 'b', 'c'];
+
+    for (index, value) in v.iter().enumerate() {
+        println!("{} is at index {}", value, index);
+    }
+    let x = 5;
+    let (x, y, z) = (1, 2, 3);
+
+    fn foo(x: i32) {
+        // code goes here
+    }
+
+    let point = (3, 5);
+    print_coordinates(&point);
+}
+fn print_coordinates(&(x, y): &(i32, i32)) {
+    println!("Current location: ({}, {})", x, y);
+}
---
+++ chapter18/refitableMatch/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("Hello, world!");
+    if let x = 5 {
+        println!("{}", x);
+    };
+}
